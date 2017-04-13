b068628d1418ea84bdd3ed70527d0365e3f3ef6e
seamusoneill <seamusoneill@live.ie>
2017-04-13 18:25:10 +0200

    Test Scene for Ergo
    Searching for nodes using radius. and limits.

--- MonsterConvoyProject/Assets/Scripts/MapScreenScripts/GenerateMapFixedNodes.cs
+++ MonsterConvoyProject/Assets/Scripts/MapScreenScripts/GenerateMapFixedNodes.cs
@@ -8,7 +8,22 @@ public class GenerateMapFixedNodes : MonoBehaviour
 
     public float searchRadius = 1.0f;
     public float searchIncrement = 1.0f;
+    public float radiusLimit = 100;
 
+    private int totalEdges;
+    //In order that the graph is planar, #nodes - #edges + #regions = 2. This must be true but it doesn't determine that there are no edge crossings if an edge cro
+    // r = 2 - n + e
+    //if #edges > 3#nodes - 6. the the graph is not planar. that's the max for a planar graph.
+
+    //Bug something is causing us to remove paths if the map doesnt work out. 
+    //Bug sometimes active node is not set. Despite their being a full nodeArr. Or at least, player is not positioned on active node. Maybe  a problem in map. maybe move positioning to player script. 
+
+        /* pseudo prevent edge crossings.
+         * Determine connection between nodes 1 & 2.
+         * Node 3 can no longer connect to any node which is between x and y of other two nodes.
+         * C'est a dire it can connect if the y position is greater than both nodes or less than both nodes. The same is true for x. one of these must be true in order to connect.
+         * If we don't want long roundabout paths then both of these must be true. 
+         */ 
     private void Start()
     {
         nodeArr = GameObject.FindGameObjectsWithTag("MapNode");
@@ -32,6 +47,12 @@ public class GenerateMapFixedNodes : MonoBehaviour
         }
         #endregion
 
+        totalEdges = totalWeight / 2;
+
+        //For the graph to be planar. 
+        int targetNumberOfRegions = 2 + totalEdges - nodeArr.Length; //As per Euler's polyhedron formula.
+        print(targetNumberOfRegions);
+
         for (int i = 0; i < nodeArr.Length; ++i)
         {
             float sR = searchRadius;
@@ -46,14 +67,13 @@ public class GenerateMapFixedNodes : MonoBehaviour
 
             //Add the nodes that have already added us to the neighhbours found list. 
             neighboursFound.AddRange(nodeArr[i].GetComponent<NodeConnections>().neighbourNodes);
-
+            
             //This can currently get stuck if there are not enough places left to connect. 
             while (neighboursFound.Count < nodeArr[i].GetComponent<NodeWeight>().weight)
             {
-                if (sR >= 100) //Prevents us from getting stuck in an infinite loop when the mapping doesn't work. TODO fix this. 
+                if (sR >= radiusLimit) //Prevents us from getting stuck in an infinite loop when the mapping doesn't work. TODO fix this. 
                     break;
 
-                sR += searchIncrement; //Note this means that the first search will always be the starting search radius + the increment.
                 for (int j = 0; j < nodeArr.Length; ++j)
                 {
                     if (i != j) //We cannot connect to ourselves.
@@ -65,6 +85,8 @@ public class GenerateMapFixedNodes : MonoBehaviour
                         }
                     }
                 }
+                sR += searchIncrement; //Increasing the nodses we're searching until e=we can determine we have enough neighbours. 
+
             }
 
             for (int j = nodeArr[i].GetComponent<NodeConnections>().neighbourNodes.Count; j < nodeArr[i].GetComponent<NodeWeight>().weight; ++j)
@@ -72,6 +94,8 @@ public class GenerateMapFixedNodes : MonoBehaviour
                 int newNeighbourIndex = Random.Range(nodeArr[i].GetComponent<NodeConnections>().neighbourNodes.Count, neighboursFound.Count);
                 nodeArr[i].GetComponent<NodeConnections>().AddNeighbour(neighboursFound[newNeighbourIndex]);
             }
+
+
         }
 
         NodeConnections.activeNode = nodeArr[0];
