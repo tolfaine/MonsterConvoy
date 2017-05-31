38b5a10a0b38c53e3ef681879f57cad11e30bd84
NAHIRI\mailan <virginie.mailan.nguyen@gmail.com>
2017-05-31 22:03:08 +0200

    System Save : To do Check wave

--- MonsterConvoyProject/Assets/Scripts/Save.cs
+++ MonsterConvoyProject/Assets/Scripts/Save.cs
@@ -8,10 +8,10 @@ using LitJson;
 public class Save : MonoBehaviour {
     JsonData _tips;
     JsonData _mutations;
-    JsonData _wave;
+    int _wave;
     // Use this for initialization
     void Start () {
-		
+        save();
 	}
 	
 	// Update is called once per frame
@@ -24,12 +24,14 @@ public class Save : MonoBehaviour {
         string filePath = Application.dataPath + "/Ressources/Save/saveFile.json";
         List<Tip> tipsToSave = GameObject.FindGameObjectWithTag("TipManager").GetComponent<TipsManager>().tipsKnownByPlayer;
         List<MutationManager.MutationData> mutationsToSave = GameObject.FindGameObjectWithTag("MutationManager").GetComponent<MutationManager>().allMutations;
+        GameObject _invasionFile = GameObject.Find("Invasion");
+        Invasion _invasion = _invasionFile.GetComponent<Invasion>();
+        int _wave = _invasion.invasionSize;
 
         _tips = JsonMapper.ToJson(tipsToSave);
         _mutations = JsonMapper.ToJson(mutationsToSave);
-        //_wave = JsonMapper.ToJson(tipsToSave);
 
-        string tmp_file = _tips.ToString() + _mutations.ToString() + _wave.ToString();
+        string tmp_file = "{ \"tips\": "+_tips.ToString() +", \"mutations\" :"+ _mutations.ToString()+", \"wave\":" + _wave +" }";
         
         if (!Directory.Exists(Application.dataPath + "/Ressources/Save"))
             Directory.CreateDirectory(Application.dataPath + "/Ressources/Save");
