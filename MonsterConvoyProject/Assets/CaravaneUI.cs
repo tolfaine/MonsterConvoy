11939e725c9d0a6db84fcc3357fc4ff4dadc50fc
seamusoneill <seamusoneill@live.ie>
2017-06-09 19:50:14 +0200

    Caravane UI size fix and scrolling arrows

--- MonsterConvoyProject/Assets/CaravaneUI.cs
+++ MonsterConvoyProject/Assets/CaravaneUI.cs
@@ -7,38 +7,70 @@ public class CaravaneUI : MonoBehaviour {
     public GameObject listMonsters;
     public GameObject prefabSlot;
 
+    public GameObject upArrow;
+    public GameObject downArrow;
+
     public Caravane caravane;
 
-	// Use this for initialization
+    int teamIndex = 0;
+    const int maxTeamDisplay = 4;
+    
 	void Start () {
+        teamIndex = 0;
         caravane = GameObject.FindGameObjectWithTag("Caravane").GetComponent<Caravane>();
         UpdateUI();
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnEnable()
+    {
+        teamIndex = 0;
+        UpdateUI(); 
+    }
 
     public void UpdateUI()
     {
-        foreach(Transform child in listMonsters.transform)
+        foreach (Transform child in listMonsters.transform)
         {
             Destroy(child.gameObject);
         }
 
-        foreach (Fighter fighter in caravane.lFighters)
+        for (int i = teamIndex; (i < caravane.lFighters.Count) && (i < teamIndex + maxTeamDisplay); i++)
         {
-
             GameObject g = Instantiate(prefabSlot, Vector3.zero, Quaternion.identity) as GameObject;
             g.transform.parent = listMonsters.transform;
 
             ImageFighterManager ifm = g.GetComponentInChildren<ImageFighterManager>();
-            ifm.UpdateImage(fighter.nID);
+            ifm.UpdateImage(caravane.lFighters[i].nID);
 
             HealthUiFighter health = g.GetComponentInChildren<HealthUiFighter>();
-            health.UpdateLife((float)fighter.nCurrentHealth / (float)fighter.nHealthMax);
+            health.UpdateLife((float)caravane.lFighters[i].nCurrentHealth / (float)caravane.lFighters[i].nHealthMax);
+        }
 
+        if (teamIndex + maxTeamDisplay < caravane.lFighters.Count)
+        { downArrow.SetActive(true); }
+        else
+        {
+            downArrow.SetActive(false);
+        }
+        if (teamIndex > 0)
+        {
+            upArrow.SetActive(true);
+        }
+        else
+        {
+            upArrow.SetActive(false);
         }
     }
+
+    public void IncreaseIndex()
+    {
+        teamIndex++;
+        UpdateUI();
+    }
+
+    public void DecreaseIndex()
+    {
+        teamIndex--;
+        UpdateUI();
+    }
+
 }
