33605b561da7fca27c27a89b50472590cd9d3991
seamusoneill <seamusoneill@live.ie>
2017-06-12 10:54:16 +0200

    Script for confirm sprite change in recrutement scene.

---
+++ MonsterConvoyProject/Assets/ConfirmTeamButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ConfirmTeamButton : MonoBehaviour {
+
+    Caravane caravane;
+	// Use this for initialization
+	void Start () {
+        caravane = GameObject.FindGameObjectWithTag("Caravane").GetComponent<Caravane>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (caravane.lFighters.Count == 4)
+        {
+            gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/HumandexIcons/Oui");
+        }
+        else
+        {
+            gameObject.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/HumandexIcons/Non");
+        }
+    }
+}
